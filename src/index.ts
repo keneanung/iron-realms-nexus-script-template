19950174f6de69c5b8ac40d9ea1c40fccc7f0d0d
keneanung <keneanung@googlemail.com>
2021-10-27 10:26:43 +0200

    Add linting for JSDoc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 // Library and NPM package entrypoint. Export all publicly visible functions here.
 
-export const Greeter = (name: string) => `Hello ${name}`;
+/**
+ * Returns a greeting for a given person.
+ *
+ * @param {string} name The name of the person you want to greet.
+ * @returns {string} A greeting for the given person.
+ */
+export const Greeter = (name: string): string => `Hello ${name}`;

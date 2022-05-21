701bd8082ca3bedfd67099282c7e9adfedb838c1
keneanung <keneanung@googlemail.com>
2022-05-21 18:01:02 +0200

    Sync with event bus

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const WebpackAutoInject = require('webpack-auto-inject-version-next');
 const configData = require('./config.js');
 
 module.exports = {
@@ -8,7 +9,30 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    useBuiltIns: 'usage',
+                    corejs: 3,
+                  },
+                ],
+                ['module:metro-react-native-babel-preset'],
+              ],
+              plugins: [
+                '@babel/plugin-transform-modules-commonjs',
+                'react-native-web',
+                ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+                ['@babel/plugin-proposal-private-methods', { loose: true }],
+              ],
+            },
+          },
+          'ts-loader',
+        ],
         exclude: /node_modules/,
       },
     ],
@@ -19,13 +43,21 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'nxs'),
-    library : {
-      type: "global",
+    library: {
+      type: 'global',
       name: configData.libraryName,
-      export: "default"
-    }
+      export: 'default',
+    },
   },
-  plugins: [new ESLintPlugin({
-    extensions: [ '.tsx', '.ts', '.js', '.jsx' ]
-  })],
+  plugins: [
+    new ESLintPlugin({
+      extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    }),
+    new WebpackAutoInject({
+      components: {
+        AutoIncreaseVersion: false,
+        InjectAsComment: false,
+      },
+    }),
+  ],
 };
